41087977491d6245594eadfc34475c57bf84c189
NunzioFornitto <76222591+NunzioFornitto@users.noreply.github.com>
2021-01-13 23:48:40 +0100

    Add files via upload

---
+++ Progetto-Rabin-Karp/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+#include "Search.h"
+
+int main()
+{
+	char txt[] = "nunzio/";
+	char pat[] = "n";
+
+	// q is prime number 
+	int q = 101;
+
+	// Function Call 
+	search(pat, txt, q);
+	return 0;
+}
+
